97a3ebb234eb35f0c4e2e70e52d9f64ecc10cecd
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-03-08 17:18:44 +0000

    dependency changes

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-/*export {
-  add,
-  multiply,
-} from "https://x.nest.land/ramda@0.27.0/source/index.js";*/
+// dependencies
 
+// std
 export * as path from "https://deno.land/std@0.89.0/path/mod.ts"
 export * as fs from "https://deno.land/std@0.89.0/fs/mod.ts"
 export * as Colors from "https://deno.land/std@0.89.0/fmt/colors.ts"
+export * as io from "https://deno.land/std@0.89.0/io/mod.ts"
+
 export { unZipFromURL } from "https://deno.land/x/zip@v1.1.0/mod.ts"
-export * as io from "https://deno.land/std/io/mod.ts"
+
