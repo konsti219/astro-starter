014ae18483b599823297e9f1969041a195cc2bec
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-03-10 13:25:36 +0000

    write Config (1/2)

--- index.ts
+++ index.ts
@@ -41,6 +41,7 @@ class Server {
         configIP: string,
         port: number,
         consolePort: string,
+        public consolePassword: string,
         public whitelist: boolean,
         public saveInterval: number,
         public backupSaves: boolean,
@@ -60,6 +61,10 @@ class Server {
         this.serverAddr = IP + ":" + port
         this.consoleAddr = IP + ":" + (consolePort === "_auto" ? port + 1 : consolePort)
 
+        this.consolePassword = this.consolePassword === "_random"
+            ? Math.random().toString(36).substring(2)
+            : this.consolePassword
+
         // configure server dir
         this.serverDir = path.join(this.starter.dir, "starterData", "servers", this.id)
         fs.ensureDirSync(this.serverDir)
@@ -76,11 +81,11 @@ class Server {
         if (this.serverType === "local") {
             fs.ensureDirSync(path.join(this.serverDir, "serverFiles"))
 
-            await this.update()
             // update
+            await this.update()
+            
             // write config
-
-            // 
+            await this.writeConfig()
 
         }
 
@@ -134,8 +139,41 @@ class Server {
         await Deno.remove(path.join(this.serverDir, "temp"), { recursive: true });
     }
 
-    writeConfig() {
+    async writeConfig() {
+        const configPath = path.join(this.serverDir, "serverFiles", "Astro", "Saved", "Config", "WindowsServer")
+        
         // AstroServerSettings.ini
+        let astroConfig = `
+[/Script/Astro.AstroServerSettings]
+bLoadAutoSave=True
+MaxServerFramerate=60
+MaxServerIdleFramerate=3
+bWaitForPlayersBeforeShutdown=False
+PublicIP=${this.serverAddr.split(":")[0]}
+ServerName=${this.name}
+MaximumPlayerCount=8
+OwnerName=${this.starter.owner}
+OwnerGuid=
+PlayerActivityTimeout=0
+ServerPassword=
+bDisableServerTravel=False
+DenyUnlistedPlayers=${this.whitelist ? "True" : "False"}
+VerbosePlayerProperties=True
+AutoSaveGameInterval=${this.saveInterval}
+BackupSaveGamesInterval=${this.backupInterval}
+ServerGuid=
+ActiveSaveFileDescriptiveName=SAVE_1
+ServerAdvertisedName=${this.name}
+ConsolePort=${this.consoleAddr.split(":")[1]}
+ConsolePassword=${this.consolePassword}
+HeartbeatInterval=55
+        `
+        for (const player of []) {
+            astroConfig += `PlayerProperties=(PlayerFirstJoinName="",PlayerCategory=Pending,PlayerGuid="",PlayerRecentJoinName="")`
+        }
+
+        await Deno.writeTextFile(path.join(configPath, "AstroServerSettings.ini"), astroConfig)
+        
         // Engine.ini
     }
 
@@ -202,6 +240,7 @@ class Starter {
                 s.IP ?? "_public",
                 s.port ?? 8777,
                 s.consolePort ?? "_auto",
+                s.consolePassword ?? "_random",
                 s.whitelist ?? false,
                 s.saveInterval ?? 900,
                 s.backupSaves ?? true,
