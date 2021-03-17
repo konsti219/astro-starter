2fc83b9703e96a7c60e74de80861e07c7c3a0d70
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-03-17 23:49:17 +0000

    handle starting connections better to avoid multiple sockets being created

--- rcon.ts
+++ rcon.ts
@@ -14,16 +14,43 @@ interface RconPlayer {
     inGame: boolean
     index: number
 }
+interface RconStats {
+    build: string
+    ownerName: string
+    maxInGamePlayers: number
+    playersInGame: number
+    playersKnownToGame: number
+    saveGameName: string
+    playerActivityTimeout: number
+    secondsInGame: number
+    serverName: string
+    serverURL: string
+    averageFPS: number
+    hasServerPassword: boolean
+    isEnforcingWhitelist: boolean
+    creativeMode: boolean
+    isAchievementProgressionDisabled: boolean
+}
+interface RconSave {
+    name: string
+    date: string
+    bHasBeenFlaggedAsCreativeModeSave: boolean
+}
 
 class RconManager {
     private encoder = new TextEncoder()
     private decoder = new TextDecoder()
-    private queue:string[] = []
+    private tempCache = ""
+
+    private queue: string[] = []
+    
     private conn?: Deno.Conn
     private reconnectInterval = 0
     public isConnected = false
 
     public players: RconPlayer[] = []
+    public stats: RconStats | undefined
+    public saves: RconSave[] = []
 
 
     constructor(private consoleAddr: string, private consolePassword: string) { }
@@ -45,12 +72,17 @@ class RconManager {
         if (!this.isConnected) {
             try {
                 // start connection
+                info("Connecting to RCON port at " + this.consoleAddr)
+
+                // set this here to prevent multiple sockets being established
+                this.isConnected = true
+
+                // get host and port and connect
                 const [hostname, port] = this.consoleAddr.split(":")
                 this.conn = await Deno.connect({ hostname, port: parseInt(port) })
 
+                // write console password
                 this.conn.write(this.encoder.encode(this.consolePassword + "\n"))
-
-                this.isConnected = true
 
                 for await (const buffer of Deno.iter(this.conn)) {
                     this.handleData(buffer)
@@ -60,28 +92,53 @@ class RconManager {
                 try {
                     this.conn?.close()
                 } catch(_) {/**/}
-                warn("Socket error/disconnect")
-                console.log(e)
+                warn("Socket error/disconnect, addr: " + this.consoleAddr)
+                // console.log(e)
             }
         }
     }
     private handleData(buffer: Uint8Array) {
-        console.log(this.decoder.decode(buffer))
-        /*
-            tempCache += data.toString();
+        // data comes in as a stream, split into the single responses
+        this.tempCache += this.decoder.decode(buffer);
 
-            if (tempCache.endsWith("\r\n")) {
-            clearTimeout(timeout);
-            client.destroy();
+        if (this.tempCache.endsWith("\n")) {
+            const parts = this.tempCache.split("\n")
 
-            next();
-            try {
-                res(JSON.parse(tempCache));
-            } catch (e) {
-                console.error("JSON failed");
-                rej(e);
+            for (const res of parts) {
+                if (res.startsWith('{"build"')) {
+                    // DSServerStatistics response
+
+                    const data:RconStats = JSON.parse(res)
+                    info("stats:")
+                    console.log(data.serverURL)
+                } else if (res.startsWith('{"activeSaveName"')) {
+                    // DSListGames response
+
+                    const data: {
+                        activeSaveName: string,
+                        gameList: RconSave[]
+                    } = JSON.parse(res)
+                    info("games:")
+                    //console.log(data)
+                } else if (res.startsWith('{"playerInfo"')) {
+                    // DSListPlayers response
+
+                    const data: {
+                        playerInfo: RconPlayer[]
+                    } = JSON.parse(res)
+                    info("players:")
+                    //console.log(data)
+                } else {
+                    if (res.length > 0) {
+                        warn("unknown res")
+                        console.log(res)
+                        console.log(res.length)
+                    }
+                }
             }
-            }*/
+
+            this.tempCache = ""
+        }
     }
 
     run(cmd:string) {
@@ -91,8 +148,15 @@ class RconManager {
     async update() {
         // "DSListPlayers"
             // DSServerStatistics
-            // DSListGamesDSListGames
-        await this.conn?.write(this.encoder.encode("DSServerStatistics\n"));
+            // DSListGames
+        info("sending to " + this.consoleAddr)
+        try {
+            await this.conn?.write(this.encoder.encode("DSServerStatistics\nDSListGames\nDSListPlayers\n"));
+        } catch (_) {
+            error("failed to send")
+            this.conn?.close()
+            this.isConnected = false
+        }
         /*return new Promise<void>((res, _) => {
             res()
         })*/
