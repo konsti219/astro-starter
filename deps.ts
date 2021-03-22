67ac5143e7fd29a5a5c7f36c6dfa049272bad8e2
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-03-22 18:43:55 +0000

    upgrade std

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 // dependencies
 
 // std
-export * as path from "https://deno.land/std@0.90.0/path/mod.ts"
-export * as fs from "https://deno.land/std@0.90.0/fs/mod.ts"
-export * as Colors from "https://deno.land/std@0.90.0/fmt/colors.ts"
-export * as io from "https://deno.land/std@0.90.0/io/mod.ts"
-export { serve } from "https://deno.land/std@0.90.0/http/server.ts"
-export { EventEmitter } from "https://deno.land/std@0.90.0/node/events.ts"
+export * as path from "https://deno.land/std@0.91.0/path/mod.ts"
+export * as fs from "https://deno.land/std@0.91.0/fs/mod.ts"
+export * as Colors from "https://deno.land/std@0.91.0/fmt/colors.ts"
+export * as io from "https://deno.land/std@0.91.0/io/mod.ts"
+export { serve } from "https://deno.land/std@0.91.0/http/server.ts"
+export { EventEmitter } from "https://deno.land/std@0.91.0/node/events.ts"
 
