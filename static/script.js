e199539a64aa956ca1a431e7602ef53957e4d9a1
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-03-23 23:07:20 +0000

    show latest version in webinterface

--- static/script.js
+++ static/script.js
@@ -1,13 +1,1 @@
 // deno-lint-ignore-file
-
-document.querySelector("#btnShutdown").addEventListener("click", async (_) => {
-    await fetch("/api/shutdown", {
-        method: "POST",
-        body: "",
-    });
-});
-
-setInterval(async () => {
-    const data = await (await fetch("/api/servers")).json();
-    console.log(data);
-}, 2000);
