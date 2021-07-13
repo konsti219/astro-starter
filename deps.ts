0d645f3fc87d73e1461fb69b5fdd68fb2181dd4b
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-07-13 15:36:12 +0200

    upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 // dependencies
 
 // std
-export * as path from "https://deno.land/std@0.98.0/path/mod.ts"
-export * as fs from "https://deno.land/std@0.98.0/fs/mod.ts"
-export * as Colors from "https://deno.land/std@0.98.0/fmt/colors.ts"
-export * as io from "https://deno.land/std@0.98.0/io/mod.ts"
-export { createHash } from "https://deno.land/std@0.98.0/hash/mod.ts";
+export * as path from "https://deno.land/std@0.101.0/path/mod.ts"
+export * as fs from "https://deno.land/std@0.101.0/fs/mod.ts"
+export * as Colors from "https://deno.land/std@0.101.0/fmt/colors.ts"
+export * as io from "https://deno.land/std@0.101.0/io/mod.ts"
+export { createHash } from "https://deno.land/std@0.101.0/hash/mod.ts";
 
 // oak
-export { Application, Router, Context, Status, isHttpError } from "https://deno.land/x/oak@v7.5.0/mod.ts"
+export { Application, Router, Context, Status, isHttpError } from "https://deno.land/x/oak@v7.7.0/mod.ts"
 // https://raw.githubusercontent.com/oakserver/oak/main/mod.ts
 
 // base64
